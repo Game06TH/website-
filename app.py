5d28b85b73d98baa07bd7bb14218314b51ef46fb
BanchaTNK <67319010047@tnk.ac.th>
2025-08-01 13:32:41 +0700

    ค.10.1

--- app.py
+++ app.py
@@ -305,6 +305,7 @@ def admin_delete_order(order_id):
     return redirect(url_for('admin_orders'))
 
 
-if __name__ == '__main__':
-        port = int(os.environ.get('PORT', 5000))
-        app.run(debug=False, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
