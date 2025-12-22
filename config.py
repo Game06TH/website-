768685bd8f0a6dff15f36b1b686894737df21f09
BanchaTNK <67319010047@tnk.ac.th>
2025-12-22 16:29:02 +0700

    t15
    เปล่ยนฐานข้อมูล

--- config.py
+++ config.py
@@ -11,16 +11,16 @@ import os
 db_config = {
     # HOST ภายในของ Render PostgreSQL
     # ใช้ค่าจริงจาก Render เป็น Default (สำหรับทดสอบในเครื่อง) หรือดึงจาก Env Var ใน Render
-    'host': os.environ.get('DB_HOST', 'dpg-d4clivf5r7bs73ag5trg-a'), # ใช้ Hostname ภายในจาก Render
+    'host': os.environ.get('DB_HOST', 'dpg-d54es7uuk2gs73b9gp30-a'), # ใช้ Hostname ภายในจาก Render
     
     # ชื่อผู้ใช้ (User)
     'user': os.environ.get('DB_USER', 'freedb_game1'), # ใช้ Username จาก Render
     
     # รหัสผ่าน
-    'password': os.environ.get('DB_PASSWORD', 'nChpySPjY547GpNSkq4g4HmQfsv718aC'), # ใช้รหัสผ่านจริงจาก Render
+    'password': os.environ.get('DB_PASSWORD', 'G0aeVp9CDAfh8GNzCdrW8aKAa0Z6jCO7'), # ใช้รหัสผ่านจริงจาก Render
     
     # ชื่อฐานข้อมูล (Database Name)
-    'database': os.environ.get('DB_NAME', 'freedb_gameth_hzr1'), # ใช้ Database Name จาก Render
+    'database': os.environ.get('DB_NAME', 'freedb_gameth_fmwb'), # ใช้ Database Name จาก Render
     
     # Port มาตรฐานของ PostgreSQL
     'port': os.environ.get('DB_PORT', 5432) # Port มาตรฐาน 5432
